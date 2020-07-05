2edcbaddb9b796e9200385f682149b410188c429
gwang.lee <gglee@amazon.com>
2020-07-05 12:55:31 -0700

    resnet_50_ibn_a_cluster

--- lib/modeling/backbones/__init__.py
+++ lib/modeling/backbones/__init__.py
@@ -4,10 +4,12 @@ from .resnext_ibn_a import resnext50_ibn_a, resnext101_ibn_a
 from .resnest import resnest50
 from .regnet.regnet import regnety_800mf, regnety_1600mf, regnety_3200mf
 from .rexnetv1 import ReXNetV1
+from .resnet_ibn_a_cluster import resnet50_ibn_a_cluster
 
 factory = {
     'resnet50': resnet50,
     'resnet50_ibn_a': resnet50_ibn_a,
+    'resnet50_ibn_a_cluster': resnet50_ibn_a_cluster,
     'resnet101_ibn_a': resnet101_ibn_a,
     'resnext101_ibn_a': resnext101_ibn_a,
     'resnest50': resnest50,
