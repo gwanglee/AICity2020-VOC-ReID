78b1b48f07dc3fa7a2bd3a4b56e94a30e71ab411
gwang.lee <gglee@amazon.com>
2020-07-12 01:11:30 -0700

    ensemble path

--- tools/aicity20/multi_model_ensemble.py
+++ tools/aicity20/multi_model_ensemble.py
@@ -8,12 +8,21 @@ from tools.aicity20.submit import write_result
 
 if __name__ == '__main__':
     # dataset = AICity20('/home/zxy/data/ReID/vehicle')
-    distmat_path = ['./output/visda/base-ensemble/dist_mat_1.npy',
-                    './output/visda/base-ensemble/dist_mat_2.npy',
+
+    distmat_path = ['./output/visda/trained/resnet50_warmup_multi_step_cm0.3_cs100_1_rerb_xbm8/distmat.py',
+                   './output/visda/trained/resnet101_warmup_multi_step_cm0.3_cs100_1_rerb_xbm8/distmat.py',
+                   './output/visda/trained/resnet50_warmup_multi_step_cm0.3_cs100_2/distmat.py',
+                   './output/visda/trained/resnext101_warmup_multi_step_cm0.3_cs100_1_rerb_xbm8/distmat.py',
+                   ]
+    # distmat_path = ['./output/visda/base-ensemble/dist_mat_1.npy',
+    #                 './output/visda/base-ensemble/dist_mat_2.npy',
+    #
+
                     # './output/visda/base-ensemble-0704/r50-E40/distmat.npy',
         #             './output/visda/base-ensemble-0704/r101-E40/distmat.npy',
         #             './output/visda/base-ensemble-0704/rx101-E40/distmat.npy',
-                    ]
+    #             ]
+
     #cam_distmat = np.load('./output/aicity20/0407-ReCamID/distmat_submit.npy')
     #ori_distmat = np.load('./output/aicity20/0409-ensemble/ReTypeID/distmat_submit.npy')
     distmat = []
@@ -24,4 +33,4 @@ if __name__ == '__main__':
 
     indices = np.argsort(distmat, axis=1)
     # write_result_with_track(indices, './output/aicity20/submit/', dataset.test_tracks)
-    write_result(indices, './output/visda/base-ensemble')
+    write_result(indices, './output/visda/trained')
